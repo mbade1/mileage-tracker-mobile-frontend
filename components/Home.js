bc8639fb6d56ab55e357b618f8411c145222e0d5
mbade1 <mbade314@gmail.com>
2020-11-13 11:38:22 -0600

    Home Screen with background image added

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,57 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, ImageBackground } from "react-native";
 
 function Home(props) {
-  return <Text styles={styles.home}>Home Test</Text>;
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../assets/home-background.jpg")}
+        style={styles.backgroundImage}
+      >
+        <Text style={styles.logo}>Running Mileage Tracker App</Text>
+        <Text style={styles.loginButton}>Log In</Text>
+        <Text style={styles.signUpButton}>Sign Up</Text>
+      </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  home: {
-    alignItems: "center",
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    bottom: 70,
+    top: 1,
+  },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    width: "100%",
+  },
+  loginButton: {
+    color: "#8a887c",
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(237, 222, 166, 0.8)",
+    bottom: 200,
+  },
+  logo: {
+    color: "#8a887c",
+    fontSize: 30,
+    bottom: 600,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(237, 222, 166, 0.8)",
+  },
+  signUpButton: {
+    color: "#8a887c",
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(237, 222, 166, 0.8)",
+    bottom: 140,
   },
 });
 
